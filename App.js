552b292375a89515fec6ec0e1da5dec36d358e2a
hamsamoh <111298812+hamsamoh@users.noreply.github.com>
2022-11-07 18:13:44 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import HomeScreen from "./HomeScreen";
+import AboutScreen from "./AboutScreen";
+import RegistrationScreen from "./RegistrationScreen"
+import LayoutRoute from './LayoutRoute';
+import LoginScreen from './LoginScreen';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <LayoutRoute path="/" component={HomeScreen} exact={true}/>
+        <LayoutRoute path="/about" component={AboutScreen} exact={true}/>
+        <LayoutRoute path="/registration" component={RegistrationScreen} exact={true}/>
+        <LayoutRoute path="/login" component={LoginScreen} exact={true}/>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
