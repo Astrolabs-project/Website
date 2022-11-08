fb247466b11883a271b625ff36161a56a93cf111
hamsamoh <111298812+hamsamoh@users.noreply.github.com>
2022-11-09 02:49:44 +0300

    Delete BootstrapButton.js

--- BootstrapButton.js
+++
@@ -1,9 +0,0 @@
-// function BootstrapButton(props) {
-//     return (
-//         <button className={"btn " + props.btnColor}>
-//             {props.children}
-//         </button>
-//     )
-// }
-
-// export default BootstrapButton;
